b9ca2fbc94001051aa3d99c3c68f58b7499a60d7
teiatan <92535482+teiatan@users.noreply.github.com>
2023-07-01 00:18:51 +0300

    fix modal closing

--- src/components/Modals/ModalCover.jsx
+++ src/components/Modals/ModalCover.jsx
@@ -1,49 +1,56 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { useEffect } from "react";
 
-export const ModalCover = ({children, onClose=()=>{}, wrapperStyles, containerStyles, buttonStyles}) => {
+export const ModalCover = ({
+  children,
+  onClose = () => {},
+  wrapperStyles,
+  containerStyles,
+  buttonStyles,
+}) => {
+  useEffect(() => {
+    const handleEscape = (e) => {
+      if (e.code === `Escape`) {
+        onClose();
+      }
+    };
+    window.addEventListener("keydown", handleEscape);
+    return () => {
+      window.removeEventListener("keydown", handleEscape);
+    };
+  }, [onClose]);
 
-    useEffect(() => {
-        const handleEscape = e => { 
-          if (e.code === `Escape`) {
-            onClose();
-          }
-        };
-        window.addEventListener('keydown', handleEscape);
-        return () => {
-          window.removeEventListener('keydown', handleEscape);
-        };
-    }, [onClose]);
+  const handleWrapperClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
 
-    return (
-        <div 
-            onMouseDown={onClose}
-            className={`fixed w-screen h-screen flex justify-center items-center bg-sky-900/50 ${wrapperStyles}`}
+  return (
+    <div
+      onMouseDown={handleWrapperClick}
+      className={`fixed w-screen h-screen flex justify-center items-center bg-sky-900/50 ${wrapperStyles}`}
+    >
+      <div
+        className={`w-[700px] h-[500px] relative flex justify-center items-center bg-cyan-100 ${containerStyles}`}
+      >
+        <button
+          onClick={onClose}
+          className={`absolute top-0 right-0 p-4 ${buttonStyles}`}
         >
+          x
+        </button>
 
-            <div 
-                className={`w-[700px] h-[500px] relative flex justify-center items-center bg-cyan-100 ${containerStyles}`}
-            >
-                
-                <button 
-                    onClick={onClose}
-                    className={`absolute top-0 right-0 p-4 ${buttonStyles}`}
-                >
-                    x
-                </button>
-
-                {children}
-
-            </div>
-
-        </div>
-    )
+        {children}
+      </div>
+    </div>
+  );
 };
 
 ModalCover.propTypes = {
-    children: PropTypes.node.isRequired,
-    onClose: PropTypes.func,
-    wrapperStyles: PropTypes.string,
-    containerStyles: PropTypes.string,
-    buttonStyles: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  onClose: PropTypes.func,
+  wrapperStyles: PropTypes.string,
+  containerStyles: PropTypes.string,
+  buttonStyles: PropTypes.string,
 };

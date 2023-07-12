84583da3d953bd77502873364b8e167d065a364b
teiatan <protas.tb@gmail.com>
2023-07-13 00:55:52 +0300

    add context hooks

---
+++ src/hooks/contextHooks.js
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { MessagesContext } from "../context/messages/messagesContext";
+import { AllRoomsContext } from "../context/rooms/allRoomsContext";
+import { ActiveRoomsContext } from "../context/rooms/activeRoomsContext";
+import { CurrentRoomContext } from "../context/rooms/currentRoomContext";
+import { UserContext } from "../context/users/userContext";
+import { OnlineUsersContext } from "../context/users/onlineUsersContext";
+import { BlackListUsersContext } from "../context/users/blacklistUsersContext";
+import { TypingUsersContext } from "../context/users/typingUsersContext";
+
+
+export const useMessages = () => useContext(MessagesContext);
+
+export const useAllRooms = () => useContext(AllRoomsContext);
+
+export const useActiveRooms = () => useContext(ActiveRoomsContext);
+
+export const useCurrentRoom = () => useContext(CurrentRoomContext);
+
+export const useUser = () => useContext(UserContext);
+
+export const useOnlineUsers = () => useContext(OnlineUsersContext);
+
+export const useBlackListUsers = () => useContext(BlackListUsersContext);
+
+export const useTypingUsers = () => useContext(TypingUsersContext);
+
